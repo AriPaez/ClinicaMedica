2aa5cd4cc50c3bff0286f475facfee83dd6d2c25
AriPaez <paezariel69@gmail.com>
2020-12-31 14:49:12 -0300

    se crea ventana medica

---
+++ src/vista/VentanaMedico.java
@@ -0,0 +1,19 @@
+package vista;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+
+public class VentanaMedico extends JFrame
+{
+	
+	public VentanaMedico()
+	{
+		setTitle("Login");
+		setBounds(300, 300, 300, 300);
+		setResizable(false);
+		setLayout(new BorderLayout());
+	}
+	
+
+}
